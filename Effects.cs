ad6fa20a17dd07d75f9bde3e765172d72d3dacd0
Chi <142235792+Chiubani@users.noreply.github.com>
2026-05-27 13:56:36 -0400

    updating effect code

--- Effects.cs
+++ Effects.cs
@@ -4,18 +4,23 @@ using System;
 public partial class Effects : TileMapLayer
 {
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	public override void _Ready(){
 		public enum number{
-			BOMB = -1;
+			BOMB = -2;
 			NONE = 0;
-			FLAG = -2;
+			FLAG = -1;
 			ONE = 1;
 			TWO = 2;
 			THREE = 3;
 			FOUR = 4;
 			FIVE = 5;
 		}
+		
+		int[][] tileValues = new int[16][16];
+		
+		int level = 1;
+		
+		for (int i = 0; i<16; i++)
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
