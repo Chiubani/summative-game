8f0b19dd99e48692360e46092590d2ab2e438a3e
Chi <142235792+Chiubani@users.noreply.github.com>
2026-05-29 14:56:55 -0400

    Moved code to scene for easier access
    + fixed array so its functional (hopefully)

--- Effects.cs
+++ Effects.cs
@@ -15,31 +15,13 @@ public partial class Effects : TileMapLayer
 			FOUR = 4;
 			FIVE = 5;
 		}
-		
-		int[][] tileValues = new int[16][16];
-		
-		int level = 1;
-		int bombLocation[][] = new int[2][55];
-		int bombsAmount = 10;
-		int bombR = 0;
-		int bombC = 0;
-		
-		for(int r = 0; r<2; r++){
-			for(int b = 0; b<bombsAmount; b++){
-				
-				do{
-					bombR = Math.round(15*Math.random()+1);
-					bombC = Math.round(15*Math.random()+1);
-				} while(bombThere(bombR,bombC,bombLocation));
-				bombLocation[bombR][bombC];
-			}
 		}
 		
-		public boolean bombThere()
+		
+		
 		
 		//Number of bombs starts at 10 and increases by 5 each level. Final level has 55 bombs
 		
-	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
---
+++ mainGameCode.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class mainGameCode : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		int level = 1;
+		int[][] gameMap = new int[15][15];
+		int[][] bombLocation = new int[2][55]; //tracks where bombs have been placed with x and y coordinates so multiple aren't placed on same square
+		int bombsAmount = 10;
+		int bombR = 0;
+		int bombC = 0;
+		
+		//Loop places bombs in 
+		for(int b = 0; b<bombsAmount; b++){
+			do{
+				bombR = Math.round(15*Math.random()+1);
+				bombC = Math.round(15*Math.random()+1);
+			} while(bombThere(bombX,bombY,bombLocation,b));
+			gameMap[bombR][bombC] = -2;
+			bombLocation[0][b] = bombR;
+			bombLocation[1][b] = bombC;
+		}
+	}
+		
+	//Making sure multiple bombs are not placed in the same square by checking if a bomb of same coordinates has been placed in one of the squares
+	public boolean bombThere(int bombR, int bombC, int arr, int b){
+		for(int i = 0; i<b;i++){
+			//if 
+			if(arr[0][i]==bombR && arr[1][i]==bombC){
+				return true;
+			}
+		}
+		return false;
+	}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
