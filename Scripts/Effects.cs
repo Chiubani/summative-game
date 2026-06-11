41e7bcda9791289d77ecb3829d9af8c86927d064
Chi <142235792+Chiubani@users.noreply.github.com>
2026-06-11 06:06:31 -0400

    Update Effects.cs

--- Scripts/Effects.cs
+++ Scripts/Effects.cs
@@ -2,17 +2,17 @@ using Godot;
 using System;
 
 public partial class Effects : TileMapLayer{
-	//public Effects(){
+	//public Effects(){}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready(){
-		public enum TileType
+		/*public enum TileType
 		{
 			Empty,
 			Bomb,
 			Treasure,
 			Forest
-		}
+		}*/
 	}
 		
 		
@@ -26,4 +26,5 @@ public partial class Effects : TileMapLayer{
 	{
 	}
 }
+//}
 //}
